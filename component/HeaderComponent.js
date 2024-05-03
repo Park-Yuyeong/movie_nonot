1814a8052057cfe07a0a6d18ea5613e1799b6f8b
delim <tdixxgidt@gmail.com>
2024-05-03 17:46:25 +0900

    header 컴포넌트 스타일 적용

--- component/HeaderComponent.js
+++ component/HeaderComponent.js
@@ -1,45 +1,43 @@
 class HeaderComponent extends HTMLElement {
   connectedCallback() {
+    // Add your HTML template
     this.innerHTML = `
-        <nav class="menubar-top">
+      <nav class="menubar-top">
+        <img id="representive-mark" src="style/image/icon.png" />
+        <form class="search-div" id="search_form">
+          <input type="text" placeholder="search..." id="searchText" autofocus />
+          <img src="style/image/search_icon.png" class="icon-search" id="handleSearch" alt="검색 아이콘" />
+        </form>
+      </nav>
+    `;
 
-            <img id="representive-mark" src="style/image/icon.png" />
-          </a>
-            
-          <form class="search-div" id="search_form">
-            <input type="text" placeholder="search..." id="searchText" autofocus />
-            <img src="style/image/search_icon.png" class="icon-search" id="handleSearch" alt="검색 아이콘" />
-          </form>
-        </nav>
-        `;
-
+    // Add your event listeners
     this.setEventReset();
     this.setEventSearch();
   }
 
-  // 로고 이미지 클릭 시 index.html로 이동
+  // Logo image click event handler to navigate to index.html
   setEventReset() {
     this.querySelector("#representive-mark").addEventListener("click", () => {
       window.location.href = "index.html";
     });
   }
 
-  // 검색 이벤트 핸들러
+  // Search event handler
   setEventSearch() {
-    const $searchButton = this.querySelector("#handleSearch"); // 검색 버튼
-    const $searchText = this.querySelector("#searchText"); // 검색어 입력창
-    const $searchForm = this.querySelector("#search_form"); // 검색 폼
+    const $searchButton = this.querySelector("#handleSearch"); // Search button
+    const $searchText = this.querySelector("#searchText"); // Search input field
+    const $searchForm = this.querySelector("#search_form"); // Search form
 
     $searchButton.addEventListener("click", (e) => {
       e.preventDefault();
       const searchText = $searchText.value.toLowerCase();
-
       window.location.href = `index.html?search=${searchText}`;
     });
+
     $searchForm.addEventListener("submit", (e) => {
       e.preventDefault();
       const searchText = $searchText.value.toLowerCase();
-
       window.location.href = `index.html?search=${searchText}`;
     });
   }
