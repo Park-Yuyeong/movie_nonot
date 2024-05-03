2c2357a92df8c0f97d0aade9e10c60d30b8fac10
Park-Yuyeong <dbduda1@kookmin.ac.kr>
2024-05-03 11:39:00 +0900

    main 검색 이벤트 핸들러 수정 & 영화 데이터 로컬에 있으면 API 안 불러오기

--- src/search.js
+++ src/search.js
@@ -1,22 +1,14 @@
-import { displayMovieData } from "./movieAPI.js";
-
-const $content = document.getElementById("eiga"); //전체 내용을 담을 컴포넌트
-const $input = document.getElementById("searchText"); //검색어입력창
-
 const movies = JSON.parse(localStorage.getItem("movies")); //영화 이름 리스트
 
 /** 검색어 함수 */
-export const search_movie = (e) => {
-  e.preventDefault();
-  const searchText = $input.value.toLowerCase();
+export const search_movie = (searchText) => {
+  console.log(searchText);
   if (!searchText.length) alert("검색창에 글자를 입력해주세요");
   else {
-    console.log(movies);
     const filteredMovieTitle = movies.filter((movie) => movie.toLowerCase().includes(searchText));
     const filteredMovieData = filteredMovieTitle.map((movie) => JSON.parse(localStorage.getItem(movie)));
     console.log(filteredMovieData);
-    $content.innerHTML = "";
 
-    displayMovieData(filteredMovieData);
+    return filteredMovieData;
   }
 };
